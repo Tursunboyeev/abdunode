cbbe7fa883fbddef63ba5212383f344c1bb6d296
Abduaziz <abdu2252112@gmail.com>
2019-04-05 20:31:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = '865127867:AAEe3AhTeah-Q967yXwzTORzbeiYlVEu_TQ'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Salom, "${msg.from.first_name}"')
+    bot.sendMessage(msg.chat.id, 'Salom, ${msg.from.first_name}')
 })
