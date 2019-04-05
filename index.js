1d2442e3eb60088f3ebc8dd39608b1bc1a43d22e
Abduaziz <abdu2252112@gmail.com>
2019-04-05 20:45:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
 
-const TOKEN = '865127867:AAEe3AhTeah-Q967yXwzTORzbeiYlVEu_TQ'
+const TOKEN = '865127867:AAEe3AhTeah-Q967yXwzTORzbeiYlVEu_TQ';
 
-const bot = new TelegramBot(TOKEN, {polling: true})
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, 'Salom, ${msg.from.first_name}')
-})
+});
