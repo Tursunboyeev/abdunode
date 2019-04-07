54689e0aca5e811d810f381425c908979ef9bed2
Abduaziz <abdu2252112@gmail.com>
2019-04-07 13:26:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const TOKEN = '865127867:AAEe3AhTeah-Q967yXwzTORzbeiYlVEu_TQ';
 
 const bot = new TelegramBot(TOKEN, {polling: true});
 
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Salom');
+});
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
