32aab0e4e96041b189cd31ae2d65e9fc08ae2601
Abduaziz <abdu2252112@gmail.com>
2019-04-07 13:13:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const TOKEN = '865127867:AAEe3AhTeah-Q967yXwzTORzbeiYlVEu_TQ';
 
 const bot = new TelegramBot(TOKEN, {polling: true});
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, 'Salom, ${msg.from.first_name}')
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, JSON.stringify(msg));
 });
